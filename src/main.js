2bbcaec3448934a0db9159880c08faf2eec281aa
Gigiiase <gigiiaseshvili94@gmail.com>
2024-11-13 12:42:56 +0400

    gigi

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+console.log(22+10);
+console.log(3*3);
+console.log("hello gigi");
+let text1 = "java";
+let text2 = "script";
+let space = " ";
+console.log(text1 + space + text2);
+
+// thats all? 
